30b3c557c14a300cae83c6d518a821f3afd9708d
Julieta Soler <julieta_soler@hotmail.com>
2026-01-29 08:56:42 -0300

    Actualizando CompNavbar & App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,28 @@ import FoundMe from './components/FoundMe'
 import MyEducation from './components/MyEducation'
 import MyProjects from './components/MyProjects'
 import Presentation from './components/Presentation'
+import CVDownloadModal from './components/CVDownloadModal'
+import { useState } from 'react'
 
-function App() {
+function App() 
+{
+  const [isCVOpen, setIsCVOpen] = useState(false)
 
   return (
     <>
       <CompHeader>
-        <CompNavbar/>
+        <CompNavbar onDownoaldCV={() => setIsCVOpen(true)}/>
       </CompHeader>
       {/*<Presentation />*/}
-      <AboutMe />
+      <AboutMe onDownoaldCV={() => setIsCVOpen(true)} />
       <MyEducation />
       <MyProjects />
       <ContactMe />
       <FoundMe />
+      <CVDownloadModal
+        isOpen={isCVOpen}
+        onClose={() => setIsCVOpen(false)}
+      />
     </>
   )
 }
