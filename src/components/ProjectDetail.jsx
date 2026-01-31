66da6dea5f7db76ce9ecd6c4279291e7c72d24c6
Julieta Soler <julieta_soler@hotmail.com>
2026-01-31 10:56:52 -0300

    ProjectDetail.jsx

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,8 +1,11 @@
-import CompSection from "./CompSection";
+import CompHeader from './CompHeader'
+import CompNavbar from './CompNavbar'
+import CompSection from './CompSection'
 
 export default function ProjectDetail()
 {
-    <CompSection>
+    <CompSection className="mt-20">
         <h1>Hola, soy el detalle de un projecto ahre</h1>
     </CompSection>
+
 }
