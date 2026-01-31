ed146e8377480b56eda2c889bb6087d5dac005f8
Julieta Soler <julieta_soler@hotmail.com>
2026-01-31 11:13:26 -0300

    actualizando ProjectDetail.jsx

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,11 +1,20 @@
 import CompSection from './CompSection'
 import { motion } from 'framer-motion'
+import Subtitle from './Subtitle'
 
 export default function ProjectDetail()
 {
     return(
         <CompSection className="mt-20">
+            <div>
+                <Subtitle subtitle="Marca" />
+            </div>
             <h1 className="text-2xl lg:text-4xl mb-5">Donde empieza la identidad</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <div>
+                <Subtitle subtitle="Isologo" />
+            </div>
+            <h2>Cuando la marca se vuelve forma</h2>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </CompSection>
     )
