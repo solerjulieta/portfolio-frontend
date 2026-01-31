936c73b3ba85ac536c29c2d04ee3439efe347d91
Julieta Soler <julieta_soler@hotmail.com>
2026-01-31 11:00:47 -0300

    fixing ProjectDetail.jsx

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -1,11 +1,11 @@
-import CompHeader from './CompHeader'
-import CompNavbar from './CompNavbar'
 import CompSection from './CompSection'
 
 export default function ProjectDetail()
 {
-    <CompSection className="mt-20">
-        <h1>Hola, soy el detalle de un projecto ahre</h1>
-    </CompSection>
+    return(
+        <CompSection className="mt-20">
+            <h1>Hola, soy el detalle de un projecto ahre</h1>
+        </CompSection>
+    )
 
 }
