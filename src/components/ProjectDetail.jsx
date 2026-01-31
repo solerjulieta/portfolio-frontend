2b81764addaa0e270199a7ee8335186916be373f
Julieta Soler <julieta_soler@hotmail.com>
2026-01-31 16:32:28 -0300

    mejorando ProjectDetail.jsx x8

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -16,7 +16,7 @@ export default function ProjectDetail()
         projectsService.getById(id)
         .then(data => {
             setProject(data)
-            console.log("La data que viene es", data)
+            console.log("El subtitulo es", data.caseStudy.subtitle[lang])
         })
     }, [id])
 
@@ -36,52 +36,24 @@ export default function ProjectDetail()
                         .sort((a, b) => a.order - b.order)
                         .map((section, index) => (
                             <motion.div
-                                    className="xl:flex xl:items-start"
-                                    initial={{ opacity: 0, y: 30 }}
-                                    whileInView={{ opacity: 1, y: 0 }}
-                                    viewport={{ once: true }}
-                                    transition={{ delay: index * 0.1 }}
-                                >
-                                    <div className="xl:mr-4">
-                                        <Subtitle subtitle={section.label?.[lang]} />
-                                    </div>
-                                    <div>
-                                        <h2 className="text-2xl lg:text-4xl mb-5">{section.title?.[lang]}</h2>
-                                        <p>{section.content}</p>
-                                    </div>
-                                </motion.div>
+                                className="xl:flex xl:items-start"
+                                initial={{ opacity: 0, y: 30 }}
+                                whileInView={{ opacity: 1, y: 0 }}
+                                viewport={{ once: true }}
+                                transition={{ delay: index * 0.1 }}
+                            >
+                                <div className="xl:mr-4">
+                                    <Subtitle subtitle={section.subtitle?.[lang]} />
+                                </div>
+                                <div>
+                                    <h2 className="text-2xl lg:text-4xl mb-5">{section.title?.[lang]}</h2>
+                                    <p>{section.content}</p>
+                                </div>
+                            </motion.div>
                         ))}
                     </div>
                 )
             }
-
-            <div className="xl:flex xl:items-start">
-                <div className="xl:mr-4">
-                    <Subtitle subtitle="Marca" />
-                </div>
-                <div>
-                    <h1 className="text-2xl lg:text-4xl mb-5">Donde empieza la identidad</h1>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-            </div>
-            <div className="flex">
-                <div className="xl:mr-4">
-                    <Subtitle subtitle="Isologo" />
-                </div>
-                <div>
-                    <h2>Cuando la marca se vuelve forma</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-            </div>
-            <div className="flex">
-                <div className="xl:mr-4">
-                    <Subtitle subtitle="Color" />
-                </div>
-                <div>
-                    <h2>Donde la emoción toma color</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                </div>
-            </div>
         </CompSection>
     )
 
