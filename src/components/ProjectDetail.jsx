6ca6acbb8771b86147451d8b2ced845232e6b5c2
Julieta Soler <julieta_soler@hotmail.com>
2026-03-05 10:03:21 -0300

    actualizando ProjectDetail

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -43,7 +43,7 @@ export default function ProjectDetail()
     }, [id])
 
     return(
-        <CompSection className="mt-20">
+        <CompSection className="my-20">
             <BackButton label={t("back_projects")} />
 
             <motion.span
