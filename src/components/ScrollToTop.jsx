3c7c00ff04b69692d9b5e17a1a491085baa3b650
Julieta Soler <julieta_soler@hotmail.com>
2026-03-05 09:45:39 -0300

    actualizando ScrollToTop

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,4 +1,4 @@
-import { AnimatePresence } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import { ChevronUp } from 'lucide-react'
 import { useEffect, useState } from 'react'
 
