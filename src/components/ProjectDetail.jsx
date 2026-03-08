507846aba852722a966baea5c142e81da394a721
Julieta Soler <julieta_soler@hotmail.com>
2026-03-08 17:42:11 -0300

    actualizando ProjectDetailx8

--- src/components/ProjectDetail.jsx
+++ src/components/ProjectDetail.jsx
@@ -94,7 +94,7 @@ export default function ProjectDetail()
                         {t("title_studycase")}
                     </motion.span>
                     <motion.h1
-                        className="text-2xl lg:text-4xl mb-2"
+                        className="text-2xl lg:text-[40px] mb-2"
                         initial={{ opacity: 0, y: 20 }}
                         animate={{ opacity: 1, y: 0 }}
                     >
@@ -142,7 +142,7 @@ export default function ProjectDetail()
                                     <Subtitle subtitle={section.subtitle?.[lang]} />
                                 </div>
                                 <div>
-                                    <h2 className="text-xl lg:text-3xl mb-6">{section.title?.[lang]}</h2>
+                                    <h2 className="text-2xl lg:text-4xl mb-6">{section.title?.[lang]}</h2>
                                     <ReactMarkDown 
                                     components={{
                                         p: ({ children }) => (
